d448b040ba08dbec6a5f45cfd6967556b024a53e
Mhyneski <magno.259716@globalcity.sti.edu.ph>
2023-12-13 14:09:07 +0800

    for hosting bug fix

--- src/components/shared/SearchResult.tsx
+++ src/components/shared/SearchResult.tsx
@@ -4,22 +4,23 @@ import GridPostList from './GridPostList';
 
 type SearchResultProps = {
   isSearchFetching: boolean;
-  searchedPosts: Models.Document[];
+  searchedPosts: { documents: Models.Document[] } | Models.Document[];
 }
 
-const SearchResult = ({isSearchFetching, searchedPosts}: SearchResultProps) => {
-  if(isSearchFetching) return <Loader />
 
-  if (searchedPosts && searchedPosts.documents.length > 0){
-    return(
+const SearchResult = ({ isSearchFetching, searchedPosts }: SearchResultProps) => {
+  if (isSearchFetching) return <Loader />
+
+  if (searchedPosts && 'documents' in searchedPosts) {
+    return (
       <GridPostList posts={searchedPosts.documents} />
-    )
-  } 
-
+    );
+  }
 
   return (
     <div className="text-light-4 mt-10 text-center w-full">no results found</div>
-  )
-} 
+  );
+}
+
 
 export default SearchResult
